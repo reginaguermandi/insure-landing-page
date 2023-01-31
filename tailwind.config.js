5f08b6cc07bc3e391638990bc513ee6baae4458d
reginaguermandi <reginaguermandi@gmail.com>
2023-01-31 15:36:00 -0300

    set intro grid layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      'dark-violet': '#2d2640',
-      'grayish-blue': '#95a9c6',
-      'very-dark-violet': '#2b272f',
-      'dark-grayish-violet': '#837d87',
-      'very-light-gray': '#fafafa',
-    },
-    fontFamily: {
-      headings: ['DM Serif Display', 'sans-serif'],
-      body: ['Karla', 'serif'],
-    },
     extend: {
+      colors: {
+        'dark-violet': '#2d2640',
+        'grayish-blue': '#95a9c6',
+        'very-dark-violet': '#2b272f',
+        'dark-grayish-violet': '#837d87',
+        'very-light-gray': '#fafafa',
+      },
+      fontFamily: {
+        headings: ['DM Serif Display', 'sans-serif'],
+        body: ['Karla', 'serif'],
+      },
       backgroundImage: {
-        'right-mobile-pattern': "url('./images/bg-pattern-intro-right-mobile.svg)",
-        'left-mobile-pattern': "url('./images/bg-pattern-intro-left-mobile.svg)",
-        'about-mobile-pattern': "url('./images/bg-pattern-how-we-work-mobile.svg)",
-        'footer-mobile-pattern': "url('./images/bg-pattern-footer-mobile.svg)",
-        'right-desktop-pattern': "url('./images/bg-pattern-intro-right-desktop.svg)",
-        'left-desktop-pattern': "url('./images/bg-pattern-intro-left-desktop.svg)",
-        'about-desktop-pattern': "url('./images/bg-pattern-how-we-work-desktop.svg)",
-        'footer-desktop-pattern': "url('./images/bg-pattern-footer-desktop.svg)"
+        rightMobilePattern: "url('./images/bg-pattern-intro-right-mobile.svg)",
+        leftMobilePattern: "url('./images/bg-pattern-intro-left-mobile.svg)",
+        aboutMobilePattern: "url('./images/bg-pattern-how-we-work-mobile.svg)",
+        footerMobilePattern: "url('./images/bg-pattern-footer-mobile.svg)",
+        rightDesktopPattern: "url('./images/bg-pattern-intro-right-desktop.svg)",
+        leftDesktopPattern: "url('./images/bg-pattern-intro-left-desktop.svg)",
+        aboutDesktopPattern: "url('./images/bg-pattern-how-we-work-desktop.svg)",
+        footerDesktopPattern: "url('./images/bg-pattern-footer-desktop.svg)",
+
       }
-    },
+    }
   },
   plugins: [],
 }
