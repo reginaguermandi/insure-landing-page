e650f036566e7d58e6e7e175ca941be15052ab3e
reginaguermandi <reginaguermandi@gmail.com>
2023-02-24 14:01:51 -0300

    feat: adds functionality to the mobile menu

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
-const openMenu = document.getElementById('#open');
-const closeMenu = document.getElementById('#close');
-const mobileMenu = document.getElementById('#mobileMenu');
+const menu = document.getElementById('menu');
+const openMenu = document.getElementById('open');
+const closeMenu = document.getElementById('close');
 
-openMenu.addEventListener('click', function (){
-    mobileMenu.classList.toggle('hidden')
-});
+openMenu.onclick = () => {
+  menu.classList.remove('hidden')
+  openMenu.classList.add('hidden')
+  closeMenu.classList.remove('hidden');
+};
+
+closeMenu.onclick = () => {
+  menu.classList.add('hidden')
+  openMenu.classList.remove('hidden')
+  closeMenu.classList.add('hidden');
+};
