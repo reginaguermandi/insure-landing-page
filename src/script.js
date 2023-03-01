e88a29b0bfda435bded7b22c4770aa71641176fa
reginaguermandi <reginaguermandi@gmail.com>
2023-03-01 17:06:36 -0300

    fix: prevent user from scrolling while menu is open

--- src/script.js
+++ src/script.js
@@ -1,36 +1,18 @@
-/*const menu = document.getElementById('menu');
-const openBtn = document.getElementById('open');
-const closeBtn = document.getElementById('close');
-
-function openMenu() {
-  menu.classList.remove('translate-x-[100%]')
-  openBtn.classList.add('hidden')
-  closeBtn.classList.remove('hidden')
-};
-
-function closeMenu() {
-  menu.classList.add('translate-x-[100%]')
-  openBtn.classList.remove('hidden')
-  closeBtn.classList.add('hidden')
-};
-
-openBtn.addEventListener('click', openMenu);
-closeBtn.addEventListener('click', closeMenu);
-menu.addEventListener('click', closeMenu);
-
-*/
 const menu = document.getElementById('menu');
 const openBtn = document.querySelector('.open')
 const closeBtn = document.querySelector('.close')
 
+// 
 function handleBtn() {
   menu.classList.toggle('translate-x-[100%]');
   openBtn.classList.toggle('hidden');
   closeBtn.classList.toggle('hidden');
+  document.body.style.overflowY = document.body.style.overflowY === 'hidden' ? 'visible' : 'hidden';
 };
 
 function openMenu() {
   handleBtn()
+
 };
 
 function closeMenu() {
@@ -38,9 +20,5 @@ function closeMenu() {
 }
 
 openBtn.addEventListener('click', handleBtn);
-closeBtn.addEventListener('click', handleBtn)
+closeBtn.addEventListener('click', handleBtn);
 menu.addEventListener('click', handleBtn);
-
-
-
-
